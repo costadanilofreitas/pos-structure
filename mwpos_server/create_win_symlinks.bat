f7ed671a1254e2d5f7e5312f6660740dda33c53f
Danilo <costa.danilofreitas@gmail.com>
2019-02-22 17:26:13 -0300

    Bat certo

--- mwpos_server/create_win_symlinks.bat
+++ mwpos_server/create_win_symlinks.bat
@@ -1,26 +1,26 @@
-mklink /D genesis\apache\darwin-x86_64 ..\mwappsdk\darwin-x86_64\apache
-mklink /D genesis\apache\linux-redhat-x86_64 ..\mwappsdk\linux-redhat-x86_64\apache
-mklink /D genesis\apache\linux-ubuntu-i686 ..\mwappsdk\linux-ubuntu-i686\apache
-mklink /D genesis\apache\linux-ubuntu-x86_64 ..\mwappsdk\linux-ubuntu-x86_64\apache
-mklink /D genesis\apache\windows-x86 ..\mwappsdk\windows-x86\apache
-mklink /D genesis\apache\windows-AMD64 ..\mwappsdk\windows-x86\apache
+mklink /D genesis\apache\darwin-x86_64 ..\..\..\mwappsdk\darwin-x86_64\apache
+mklink /D genesis\apache\linux-redhat-x86_64 ..\..\..\mwappsdk\linux-redhat-x86_64\apache
+mklink /D genesis\apache\linux-ubuntu-i686 ..\..\..\mwappsdk\linux-ubuntu-i686\apache
+mklink /D genesis\apache\linux-ubuntu-x86_64 ..\..\..\mwappsdk\linux-ubuntu-x86_64\apache
+mklink /D genesis\apache\windows-x86 ..\..\..\mwappsdk\windows-x86\apache
+mklink /D genesis\apache\windows-AMD64 ..\..\..\mwappsdk\windows-x86\apache
 
 
-mklink /D genesis\bin\darwin-x86_64 ..\mwappsdk\darwin-x86_64\bin
-mklink /D genesis\bin\linux-redhat-x86_64 ..\mwappsdk\linux-redhat-x86_64\bin
-mklink /D genesis\bin\linux-ubuntu-i686 ..\mwappsdk\linux-ubuntu-i686\bin
-mklink /D genesis\bin\linux-ubuntu-x86_64 ..\mwappsdk\linux-ubuntu-x86_64\bin
-mklink /D genesis\bin\windows-x86 ..\mwappsdk\windows-x86\bin
-mklink /D genesis\bin\windows-AMD64 ..\mwappsdk\windows-x86\bin
+mklink /D genesis\bin\darwin-x86_64 ..\..\..\mwappsdk\darwin-x86_64\bin
+mklink /D genesis\bin\linux-redhat-x86_64 ..\..\..\mwappsdk\linux-redhat-x86_64\bin
+mklink /D genesis\bin\linux-ubuntu-i686 ..\..\..\mwappsdk\linux-ubuntu-i686\bin
+mklink /D genesis\bin\linux-ubuntu-x86_64 ..\..\..\mwappsdk\linux-ubuntu-x86_64\bin
+mklink /D genesis\bin\windows-x86 ..\..\..\mwappsdk\windows-x86\bin
+mklink /D genesis\bin\windows-AMD64 ..\..\..\mwappsdk\windows-x86\bin
 
 
 
-mklink /D genesis\python\darwin-x86_64 ..\mwappsdk\darwin-x86_64\python
-mklink /D genesis\python\linux-redhat-x86_64 ..\mwappsdk\linux-redhat-x86_64\python
-mklink /D genesis\python\linux-ubuntu-i686 ..\mwappsdk\linux-ubuntu-i686\python
-mklink /D genesis\python\linux-ubuntu-x86_64 ..\mwappsdk\linux-ubuntu-x86_64\python
-mklink /D genesis\python\windows-x86 ..\mwappsdk\windows-x86\python
-mklink /D genesis\python\windows-AMD64 ..\mwappsdk\windows-x86\python
+mklink /D genesis\python\darwin-x86_64 ..\..\..\mwappsdk\darwin-x86_64\python
+mklink /D genesis\python\linux-redhat-x86_64 ..\..\..\mwappsdk\linux-redhat-x86_64\python
+mklink /D genesis\python\linux-ubuntu-i686 ..\..\..\mwappsdk\linux-ubuntu-i686\python
+mklink /D genesis\python\linux-ubuntu-x86_64 ..\..\..\mwappsdk\linux-ubuntu-x86_64\python
+mklink /D genesis\python\windows-x86 ..\..\..\mwappsdk\windows-x86\python
+mklink /D genesis\python\windows-AMD64 ..\..\..\mwappsdk\windows-x86\python
 
 
-mklink /D genesis\data\server ..\mwdatas\data.client\server
+mklink /D genesis\data\server ..\..\..\mwdatas\data.client\server
