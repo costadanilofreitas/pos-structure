fad91bb2f4d1c0657462e59917788770dcdf7929
Danilo <costa.danilofreitas@gmail.com>
2019-02-22 17:48:14 -0300

    Fixed symlinks paths. Added variables to teste.bat

--- mwpos_server/teste.bat
+++ mwpos_server/teste.bat
@@ -5,7 +5,8 @@ if "%ERRORLEVEL%"=="0" (exit)
 tasklist /FI "IMAGENAME eq hv.exe" 2>NUL | find /I "hv.exe" >NUL
 if "%ERRORLEVEL%"=="0" (exit)
 
-set DEBUGGER=true
+set DEBUGGER=false
+set DEBUG=true
 set PYDEVPATH=%CD%\pycharm-debug.egg
 set MWPOS_BIN=%CD%\bin
 set APACHE_MODS=%CD%\apache\modules
@@ -14,6 +15,8 @@ set DATADIR=%CD%\data
 set HVLOGFILE=%DATADIR%\logs\hv.log
 set HVMAXLOGFILES=20
 set PATH=%PYTHONHOME%;%PYTHONHOME%\bin;%PYTHONHOME%\lib;%PGSQLHOME%\bin;%PGSQLHOME%\lib;%APACHE_MODS%;%PATH%
+set HVLOGLEVEL=63
+set HVSTATS=1
 IF "%NODEID%"=="" set NODEID=server
 
 IF NOT EXIST "%MWPOS_BIN%\genclient.exe" call :ERROR Could not find hypervisor.
