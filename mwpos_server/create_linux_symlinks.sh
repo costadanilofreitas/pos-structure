4c2be8789176fc5a9292d30655e80fda5ebb8e12
Danilo <costa.danilofreitas@gmail.com>
2019-02-25 17:34:34 -0300

    correção de script

--- mwpos_server/create_linux_symlinks.sh
+++ mwpos_server/create_linux_symlinks.sh
@@ -1,43 +1,43 @@
-unlink genesis\apache\darwin-x86_64
-ln -s genesis\apache\darwin-x86_64 ..\..\..\mwappsdk\darwin-x86_64\apache
-unlink genesis\apache\linux-redhat-x86_64
-ln -s genesis\apache\linux-redhat-x86_64 ..\..\..\mwappsdk\linux-redhat-x86_64\apache
-unlink genesis\apache\linux-ubuntu-i686
-ln -s genesis\apache\linux-ubuntu-i686 ..\..\..\mwappsdk\linux-ubuntu-i686\apache
-unlink genesis\apache\linux-ubuntu-x86_64
-ln -s genesis\apache\linux-ubuntu-x86_64 ..\..\..\mwappsdk\linux-ubuntu-x86_64\apache
-unlink genesis\apache\windows-x86
-ln -s genesis\apache\windows-x86 ..\..\..\mwappsdk\windows-x86\apache
-unlink genesis\apache\windows-AMD64
-ln -s genesis\apache\windows-AMD64 ..\..\..\mwappsdk\windows-x86\apache
+unlink genesis/apache/darwin-x86_64
+ln -s genesis/apache/darwin-x86_64 ../../../mwappsdk/darwin-x86_64/apache
+unlink genesis/apache/linux-redhat-x86_64
+ln -s genesis/apache/linux-redhat-x86_64 ../../../mwappsdk/linux-redhat-x86_64/apache
+unlink genesis/apache/linux-ubuntu-i686
+ln -s genesis/apache/linux-ubuntu-i686 ../../../mwappsdk/linux-ubuntu-i686/apache
+unlink genesis/apache/linux-ubuntu-x86_64
+ln -s genesis/apache/linux-ubuntu-x86_64 ../../../mwappsdk/linux-ubuntu-x86_64/apache
+unlink genesis/apache/windows-x86
+ln -s genesis/apache/windows-x86 ../../../mwappsdk/windows-x86/apache
+unlink genesis/apache/windows-AMD64
+ln -s genesis/apache/windows-AMD64 ../../../mwappsdk/windows-x86/apache
 
 
-unlink genesis\bin\darwin-x86_64
-ln -s genesis\bin\darwin-x86_64 ..\..\..\mwappsdk\darwin-x86_64\bin
-unlink genesis\bin\linux-redhat-x86_64
-ln -s genesis\bin\linux-redhat-x86_64 ..\..\..\mwappsdk\linux-redhat-x86_64\bin
-unlink genesis\bin\linux-ubuntu-i686
-ln -s genesis\bin\linux-ubuntu-i686 ..\..\..\mwappsdk\linux-ubuntu-i686\bin
-unlink genesis\bin\linux-ubuntu-x86_64
-ln -s genesis\bin\linux-ubuntu-x86_64 ..\..\..\mwappsdk\linux-ubuntu-x86_64\bin
-unlink genesis\bin\windows-x86
-ln -s genesis\bin\windows-x86 ..\..\..\mwappsdk\windows-x86\bin
-unlink genesis\bin\windows-AMD64
-ln -s genesis\bin\windows-AMD64 ..\..\..\mwappsdk\windows-x86\bin
+unlink genesis/bin/darwin-x86_64
+ln -s genesis/bin/darwin-x86_64 ../../../mwappsdk/darwin-x86_64/bin
+unlink genesis/bin/linux-redhat-x86_64
+ln -s genesis/bin/linux-redhat-x86_64 ../../../mwappsdk/linux-redhat-x86_64/bin
+unlink genesis/bin/linux-ubuntu-i686
+ln -s genesis/bin/linux-ubuntu-i686 ../../../mwappsdk/linux-ubuntu-i686/bin
+unlink genesis/bin/linux-ubuntu-x86_64
+ln -s genesis/bin/linux-ubuntu-x86_64 ../../../mwappsdk/linux-ubuntu-x86_64/bin
+unlink genesis/bin/windows-x86
+ln -s genesis/bin/windows-x86 ../../../mwappsdk/windows-x86/bin
+unlink genesis/bin/windows-AMD64
+ln -s genesis/bin/windows-AMD64 ../../../mwappsdk/windows-x86/bin
 
 
-unlink genesis\python\darwin-x86_64
-ln -s genesis\python\darwin-x86_64 ..\..\..\mwappsdk\darwin-x86_64\python
-unlink genesis\python\linux-redhat-x86_64
-ln -s genesis\python\linux-redhat-x86_64 ..\..\..\mwappsdk\linux-redhat-x86_64\python
-unlink genesis\python\linux-ubuntu-i686
-ln -s genesis\python\linux-ubuntu-i686 ..\..\..\mwappsdk\linux-ubuntu-i686\python
-unlink genesis\python\linux-ubuntu-x86_64
-ln -s genesis\python\linux-ubuntu-x86_64 ..\..\..\mwappsdk\linux-ubuntu-x86_64\python
-unlink genesis\python\windows-x86
-ln -s genesis\python\windows-x86 ..\..\..\mwappsdk\windows-x86\python
-unlink genesis\python\windows-AMD64
-ln -s genesis\python\windows-AMD64 ..\..\..\mwappsdk\windows-x86\python
+unlink genesis/python/darwin-x86_64
+ln -s genesis/python/darwin-x86_64 ../../../mwappsdk/darwin-x86_64/python
+unlink genesis/python/linux-redhat-x86_64
+ln -s genesis/python/linux-redhat-x86_64 ../../../mwappsdk/linux-redhat-x86_64/python
+unlink genesis/python/linux-ubuntu-i686
+ln -s genesis/python/linux-ubuntu-i686 ../../../mwappsdk/linux-ubuntu-i686/python
+unlink genesis/python/linux-ubuntu-x86_64
+ln -s genesis/python/linux-ubuntu-x86_64 ../../../mwappsdk/linux-ubuntu-x86_64/python
+unlink genesis/python/windows-x86
+ln -s genesis/python/windows-x86 ../../../mwappsdk/windows-x86/python
+unlink genesis/python/windows-AMD64
+ln -s genesis/python/windows-AMD64 ../../../mwappsdk/windows-x86/python
 
-unlink genesis\data\server
-ln -s genesis\data\server ..\..\..\mwdatas\data.client\server
+unlink genesis/data/server
+ln -s genesis/data/server ../../../mwdatas/data.client/server
