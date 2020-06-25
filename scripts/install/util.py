090d2fe18e80e43726a2ab41ca0ea7b21879f8c0
Danilo <costa.danilofreitas@gmail.com>
2020-06-24 22:31:51 -0300

    Added configureApache.py

--- scripts/install/util.py
+++ scripts/install/util.py
@@ -43,7 +43,7 @@ def logger(fn):
 
 
 class Util(object):
-    def __init__(self, new_package=False):
+    def __init__(self, new_package=False, installingApache=False):
         configurations = self.get_configurations()
 
         self.pos_folder_name = configurations["pos_folder_name"]
@@ -57,7 +57,11 @@ class Util(object):
         self.install_folder = os.path.join(self.current_folder, 'install')
 
         self.backup_folder = os.path.join(self.current_folder, (self.pos_folder_name + "_backup"))
+
         self.e_deploy_pos_folder = os.path.join(self.current_folder, self.pos_folder_name)
+        if installingApache:
+            self.e_deploy_pos_folder = self.current_folder
+
         if new_package:
             self.e_deploy_pos_folder = self.e_deploy_pos_folder + "_downloaded"
 
