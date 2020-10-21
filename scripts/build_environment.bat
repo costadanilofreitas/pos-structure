e18b4cf17293d0ceebc65e6617f88f4bfec24699
Danilo <costa.danilofreitas@gmail.com>
2020-10-21 15:53:14 -0300

    Adding build environment variables to build_environment.bat

--- scripts/build_environment.bat
+++ scripts/build_environment.bat
@@ -1,3 +1,16 @@
-@echo off
-git-bash -c "./make.sh"
-exit
+REM @echo off
+call set_paths.bat
+IF "%PYTHON_DIR%"=="" (
+  set PYTHON_DIR=C:\Python27
+)
+
+IF "%GIT_DIR%"=="" (
+set GIT_DIR=C:\Program Files\Git
+)
+
+IF "%NODE_DIR%"=="" (
+set NODE_DIR=C:\Program Files\nodejs
+)
+
+set PATH=%PYTHON_DIR%;%PYTHON_DIR%\Scripts;%GIT_DIR%;%GIT_DIR%\cmd\;%NODE_DIR%;%PATH%
+REM git-bash -c "./make.sh"
