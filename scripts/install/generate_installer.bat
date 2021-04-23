e0460ff54e63a1da7dcbe2293e26f4bac5a8984a
Danilo <costa.danilofreitas@gmail.com>
2021-04-23 11:18:46 -0300

    Including python folder in installer / Added an icon on exe

--- scripts/install/generate_installer.bat
+++ scripts/install/generate_installer.bat
@@ -2,15 +2,17 @@ if exist dist (
     rmdir /s /q "dist"
 )
 
-pyinstaller --onefile installEdeployPOS.py
+pyinstaller --onefile --icon="icon.png" installEdeployPOS.py
 
 if exist install (
     rmdir /s /q "install"
 )
 mkdir "install"
-copy "dist" "install"
+xcopy "dist" "install" /E
 mkdir "install\pos_files"
-copy "pos_files" "install/pos_files"
+xcopy "pos_files" "install/pos_files" /E
 copy "configurations.txt" "install"
+mkdir "install\python"
+xcopy "..\..\mwsdk\windows-x86\python" "install/python" /E
 
 tar.exe -a -c -f install.zip install
