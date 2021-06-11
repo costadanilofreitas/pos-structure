de9038a2a41abdb1b17c992c0fc43593ba669fdc
Danilo <costa.danilofreitas@gmail.com>
2021-06-11 08:30:21 -0300

    Updating start.bat / fix price_over_write authorization / Update CORE to 3.1.1

--- start.bat
+++ start.bat
@@ -1,52 +1,49 @@
 @echo off
 
-SET CurDir=%CD%
-cd datas\server\databases
-if exist i18ncustom.db (
-    del i18ncustom.db
-)
-cd %CurDir%
+tasklist /FI "IMAGENAME eq genclient.exe" 2>NUL | find /I "genclient.exe" >NUL
+if "%ERRORLEVEL%"=="0" (exit)
+tasklist /FI "IMAGENAME eq hv.exe" 2>NUL | find /I "hv.exe" >NUL
+if "%ERRORLEVEL%"=="0" (exit)
 
-set MWAPP_NODE=server
+
+set APP_NODE=server
 set PLATFORM=windows-x86
 set HVMAXLOGFILES=5
 set HVLOGLEVEL=63
 set BASEDIR=%CD%
 set DEBUG=true
 
-IF NOT EXIST %BASEDIR%\genesis call :ERROR Could not find 'genesis' in base dir.
-IF NOT EXIST %BASEDIR%\mwsdk call :ERROR Could not find 'mwsdk' in base dir.
-
+REM if exist %BASEDIR%\genesis goto :rungenclient
 if exist %BASEDIR%\mwsdk goto :runhv
 goto :EXIT
 
 :rungenclient
 set BASEBINDIR=%BASEDIR%
-set MWDATASDIR=%BASEDIR%\..\datas
+set DATADIR=%BASEDIR%\..\datas
 set EXECUTABLE=genclient.exe
 goto :setupenv
 
 :runhv
 set BASEBINDIR=%BASEDIR%\mwsdk\%PLATFORM%
-set MWDATASDIR=%BASEDIR%\datas
+set DATADIR=%BASEDIR%\datas
 set EXECUTABLE=hv.exe
 goto :setupenv
 
 :setupenv
-set MWPOS_BIN=%BASEBINDIR%\bin
+set POS_BIN=%BASEBINDIR%\bin
 set APACHE_MODS=%BASEBINDIR%\apache\modules
 set PYTHONHOME=%BASEBINDIR%\python
-set DATADIR=%MWDATASDIR%\
+set DATADIR=%DATADIR%\
 set HVLOGFILE=%DATADIR%\server\logs\hv.log
 set PATH=%PYTHONHOME%;%PYTHONHOME%\bin;%PYTHONHOME%\lib;%APACHE_MODS%;%PATH%
 
-IF NOT EXIST %MWPOS_BIN%\%EXECUTABLE% call :ERROR Could not find exexutable %EXECUTABLE%.
+IF NOT EXIST %POS_BIN%\%EXECUTABLE% call :ERROR Could not find executable %EXECUTABLE%.
 
-cd %MWPOS_BIN%
+cd %POS_BIN%
 :: Run genclient
-if [%EXECUTABLE%]==[genclient.exe] call :EXEC genclient.exe --local ..\genesis --notime 1 --force 1 --node %MWAPP_NODE% 
+if [%EXECUTABLE%]==[genclient.exe] call :EXEC genclient.exe --local ..\genesis --data ..\data\server --notime 1 --force 1 --node %APP_NODE% 
 :: Run hv
-if [%EXECUTABLE%]==[hv.exe] call :EXEC hv.exe --service --data "%DATADIR%\%MWAPP_NODE%"
+if [%EXECUTABLE%]==[hv.exe] call :EXEC hv.exe --service --data "%DATADIR%\%APP_NODE%"
 
 :: Function that executes a command and checks errorlevel
 :EXEC
